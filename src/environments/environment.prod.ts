ca46cc9206159470af9baab9f2e297476ed00630
FabriJuncal <juncalfabri@gmail.com>
2025-02-25 13:27:35 -0300

    Primer commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,20 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Grocery Delivery App Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2024-present initappz.
+*/
+export const environment = {
+  production: true,
+  baseUrl: "localhost/public/api/",
+  imageUrl: "localhost/public/storage/images/",
+  firebase: {
+    apiKey: "key",
+    authDomain: "key",
+    projectId: "key",
+    storageBucket: "key",
+    messagingSenderId: "key",
+    appId: "key",
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,20 @@
+/*
+  Authors : initappz (Rahul Jograna)
+  Website : https://initappz.com/
+  App Name : Grocery Delivery App Source code is licensed as per the
+  terms found in the Website https://initappz.com/license
+  Copyright and Good Faith Purchasers © 2024-present initappz.
+*/
+export const environment = {
+  production: false,
+  baseUrl: "https://localhost/public/api/",
+  imageUrl: "https://localhost/public/storage/images/",
+  firebase: {
+    apiKey: "key",
+    authDomain: "key",
+    projectId: "key",
+    storageBucket: "key",
+    messagingSenderId: "key",
+    appId: "key",
+  },
+};
